8201a86ed0bfb83385a4b811f028f7a030cb925f
Daniel Bundor <danielbundor91@gmail.com>
2023-01-09 20:01:39 -0600

    pushing pom.xml file

---
+++ src/main/java/com/dan/app/App.java
@@ -0,0 +1,13 @@
+package com.dan.app;
+
+/**
+ * "Hello world! this is Daniel Bundor learning Maven"
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
